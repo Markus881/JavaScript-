a390d276a0bc072e08c1212ece825e492955dcce
Markus881 <ivanbogoslavets@ukr.net>
2022-11-09 10:15:59 +0200

    make hw2

---
+++ hw2/script.js
@@ -0,0 +1,15 @@
+//Пункт-1!
+const userFirstName = 'Ivan';
+const userLastName = 'Bogoslavets';
+let userAge = 34;
+
+//Пункт-2
+let userNew;
+let listShopping;
+let userStatus;
+let employeeSalary;
+let cursorCoordinates;
+
+//Пункт-3
+console.log('Hello, world!');
+console.log(userAge);
