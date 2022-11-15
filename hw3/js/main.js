8f9ddc2213887d9dda83226499bf2c2921a06c65
Markus881 <ivanbogoslavets@ukr.net>
2022-11-15 19:34:01 +0200

    change js

---
+++ hw3/js/main.js
@@ -0,0 +1,25 @@
+//Завдання 1
+
+const a = 5;
+const b = 10;
+const c = a + b;
+
+console.log(c);
+
+const a1 = 5;
+const b1 = 10;
+const c1 = a1 - b1;
+
+console.log(c1);
+
+const a2 = 5;
+const b2 = 10;
+const c2 = a2 * b2;
+
+console.log(c2);
+
+const a3 = 5;
+const b3 = 10;
+const c3 = a3 / b3;
+
+console.log(c3);
--- hw3/js/script.js
+++ hw3/js/script.js
@@ -1,32 +1,9 @@
-//Завдання 1
+//Завдання 3
+var a4 = +prompt('Введіть перше число!', '');
+var b4 = Number(prompt('Введіть друге число!', ''));
+var c4 = a4 < b4;
+alert(c4);
 
-const a = 5;
-const b = 10;
-const c = a + b;
-c;
-
-console.log('c');
-
-//Завдання 2
-
-true && true //true
-false || true //true
-true && 'false' //false
-0 && 1 || ' ' && 42 //42
-null == undefined  //true
-'' == 0 // true
-1 === '1' //false
-NaN === NaN && true //false
-true || false && true || false //true
-(true || false) && (true || false) //true
-10 || 0 && 'dog' || '' //10
-(10 || 0) && ('dog' || '') //'dog'
-
-var a = +prompt('Введіть перше число!', '');
-var b = Number(prompt('Введіть друге число!', ''));
-var c = a < b;
-alert(c);
-
-if (a < b) {
+if (a4 < b4) {
     rusult = true;
 }
