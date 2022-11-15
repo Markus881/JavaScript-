43788b01fdf681c75122d9db70eb3e407a6934eb
Markus881 <ivanbogoslavets@ukr.net>
2022-11-15 19:10:15 +0200

    commit

---
+++ hw3/script.js
@@ -0,0 +1,24 @@
+//Завдання 1
+
+const a = 5;
+const b = 10;
+const c = a + b;
+c;
+
+console.log('c');
+
+//Завдання 2
+
+true && true //true
+false || true //true
+true && 'false' //false
+0 && 1 || ' ' && 42 //42
+null == undefined  //true
+'' == 0 // true
+1 === '1' //false
+NaN === NaN && true //false
+true || false && true || false //true
+(true || false) && (true || false) //true
+10 || 0 && 'dog' || '' //10
+(10 || 0) && ('dog' || '') //'dog'
+
