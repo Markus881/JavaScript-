18d762f0487ff0bb45839b7136b264c84d62ecec
Іван <109347484+Markus881@users.noreply.github.com>
2022-11-21 10:30:40 +0200

    commit

--- hw5/script.js
+++ hw5/script.js
@@ -25,10 +25,10 @@ do {
 
 //Завдання 2
 
-let n = 10;
+let $n = 10;
 
 nextPrime:
-for (let $i = 2; $i <= n; $i++) { 
+for (let $i = 2; $i <= $n; $i++) { 
 
   for (let j = 2; j < $i; j++) { 
     if ($i % j == 0) continue nextPrime; 
