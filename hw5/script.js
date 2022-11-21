7e16682c10a78a654511b10ab78c3bf5dba0947a
Іван <109347484+Markus881@users.noreply.github.com>
2022-11-21 10:29:43 +0200

    commit

---
+++ hw5/script.js
@@ -0,0 +1,52 @@
+//Завдання 1
+
+// Цикл for
+
+for (let b = 0; b < 10; b++) {
+    alert(b);
+  }
+
+// Цикл while
+
+let a = 10;
+while (a) { 
+  alert( a );
+  a--;
+}
+
+// Цикл “do…while”
+
+let c = 0;
+do {
+  alert( c );
+  c++;
+} while (c < 10);
+
+
+//Завдання 2
+
+let n = 10;
+
+nextPrime:
+for (let $i = 2; $i <= n; $i++) { 
+
+  for (let j = 2; j < $i; j++) { 
+    if ($i % j == 0) continue nextPrime; 
+  }
+
+  alert( $i ); 
+}
+
+// ============
+
+let n = 100;
+
+nextPrime:
+for (let i = 21; i <= n; i++) { 
+
+  for (let j = 21; j < i; j++) { 
+    if (i % j == 0) continue nextPrime; 
+  }
+
+  alert( i ); 
+}
