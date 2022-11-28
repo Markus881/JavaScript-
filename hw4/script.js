abf32cb5d59675e4784ac546d5f4726b29045789
Markus881 <ivanbogoslavets@ukr.net>
2022-11-28 20:58:06 +0200

    commit hw3 task 3

--- hw4/script.js
+++ hw4/script.js
@@ -1,25 +1,25 @@
 // Завдання 1
 
-function comparison(a, b) {
-    return a == b;
-}
-let result = comparison(+prompt('Введіть перше число!', ''), +prompt('Введіть друге число!', ''));  
+// function comparison(a, b) {
+//     return a == b;
+// }
+// let result = comparison(+prompt('Введіть перше число!', ''), +prompt('Введіть друге число!', ''));  
   
-  alert( result );
+//   alert( result );
 
 //Завдання 2
 
-function getSum(num1, num2){
-num1 = Number(prompt('Введіть перше число!', ''));
-num2 = Number(prompt('Введіть друге число!', ''));
-sum = num1 + num2;
-alert(sum)
-}
-getSum();
-// ====
-let sum = (a, b) => a + b;
+// function getSum(num1, num2){
+// num1 = Number(prompt('Введіть перше число!', ''));
+// num2 = Number(prompt('Введіть друге число!', ''));
+// sum = num1 + num2;
+// alert(sum)
+// }
+// getSum();
+// // ====
+// let sum = (a, b) => a + b;
 
-alert( sum(1, 2) );
+// alert( sum(1, 2) );
 
 // Завдання 3
 
@@ -27,20 +27,20 @@ function exponentiation (a, b = 2) {
     let num = Number(prompt('Введіть число!', ''));
     alert (num**2);
 }
-exponentiation (a, b = 2);
+exponentiation ();
 
 // Завдання 4
 
-let Day = function (day) {
-    switch (day) {
-        case 1: return "Понеділок";
-        case 2: return "Вівторок";
-        case 3: return "Середа";
-        case 4: return "Четверг";
-        case 5: return "П'ятниця";
-        case 6: return "Субота";
-        case 7: return "Неділя";
-        default: return "Ви ввели некоректне число!"
-    }
-}
-document.write(Day(0));
+// let Day = function (day) {
+//     switch (day) {
+//         case 1: return "Понеділок";
+//         case 2: return "Вівторок";
+//         case 3: return "Середа";
+//         case 4: return "Четверг";
+//         case 5: return "П'ятниця";
+//         case 6: return "Субота";
+//         case 7: return "Неділя";
+//         default: return "Ви ввели некоректне число!"
+//     }
+// }
+// document.write(Day(0));
